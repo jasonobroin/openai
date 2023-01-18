a3e7381887ea3765776873f4a217f2b6a87a3e18
Jason O'Broin <jason@obroin.net>
2023-01-17 22:25:05 -0800

    Allow model and temperature to be set from the command line

--- ai.py
+++ ai.py
@@ -26,8 +26,6 @@ from datetime import datetime, timedelta, timezone
 
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-model_engine = "text-davinci-003" # "chatGPT"
-
 _print = print
 def pprint(*args, **kw):
     _print("[%s]" % (datetime.now()),*args, **kw)
@@ -40,14 +38,16 @@ def get_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
-    parser.add_argument("-m", "--models", help="List available models", action="store_true")
+    parser.add_argument("-l", "--list-models", help="List available models", action="store_true")
+    parser.add_argument("-t", "--temperature", help="Set the model's temperature from 0 to 1. 0 is more predictable; 1 more creative", type=float, default=0.6)
+    parser.add_argument("-m", "--model", help="Select the model to use", default="text-davinci-003")
 
     return parser.parse_args()
 
 def list_models():
     '''List the various model types - current flat, but would be interesting to show hierarchy'''
     models = openai.Model.list()
-    print(models)
+#    print(models)
 
     for model in models["data"]:
         print(model["id"])
@@ -57,14 +57,15 @@ def main():
 
     args = get_args()
 
-    if args.models == True:
+    if args.list_models == True:
         list_models()
 
     while True:
         # Set the prompt and generate text
         prompt = input('openai> ')
         #prompt = "What is the best way to work out the value of PI"
-        completions = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1024, n=1, stop=None, temperature=0.5)
+        completions = openai.Completion.create(engine=args.model, prompt=prompt, max_tokens=1024, n=1, stop=None,
+                                               temperature=args.temperature)
         message = completions.choices[0].text
 
         paragraph = str.splitlines(message)
